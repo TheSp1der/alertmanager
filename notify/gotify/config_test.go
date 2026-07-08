ee0d8358ab087624defd6501b668a98aa6a7356e
nhyatt <nhyatt@smoothnet.org>
2026-07-08 16:24:00 -0500

    feat(notify): add Gotify notifier implementation
    Signed-off-by: nhyatt <nhyatt@smoothnet.org>

---
+++ notify/gotify/config_test.go
@@ -0,0 +1,85 @@
+package gotify
+
+import (
+	"errors"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	"gopkg.in/yaml.v2"
+)
+
+func TestGotifyConfig_UnmarshalYAML(t *testing.T) {
+	test := []struct {
+		name     string
+		in       string
+		expected error
+	}{
+		{
+			name: "with url and token - successful run",
+			in: `
+url: http://localhost:3000
+token: 00000000-0000-0000-0000-0000000000001
+`,
+		}, {
+			name: "with url_file and token_file - successful run",
+			in: `
+url_file: /path/to/file
+token_file: /path/to/token
+`,
+		}, {
+			name: "with url and token_file - successful run",
+			in: `
+url: http://localhost:3000
+token_file: /path/to/token
+`,
+		}, {
+			name: "with url_file and token - successful run",
+			in: `
+url_file: /path/to/file
+token: 00000000-0000-0000-0000-0000000000001
+`,
+		}, {
+			name: "missing url and url_file, token provided - expected error missing url or url_file",
+			in: `
+token: 00000000-0000-0000-0000-0000000000001
+`,
+			expected: errors.New("one of url or url_file must be configured"),
+		}, {
+			name: "missing token and token_file, url provided - expected error missing token or token_file",
+			in: `
+url: http://localhost:3000
+`,
+			expected: errors.New("one of token or token_file must be configured"),
+		},{
+			name: "url and url_file provided - expected error at most one of url & url_file",
+			in: `
+url: http://localhost:3000
+url_file: /path/to/file
+`,
+			expected: errors.New("at most one of url & url_file must be configured"),
+		},{
+			name: "token and token_file provided - expected error at most one of token & token_file",
+			in: `
+url: http://localhost:3000
+token: 00000000-0000-0000-0000-0000000000001
+token_file: /path/to/token
+`,
+			expected: errors.New("at most one of token & token_file must be configured"),
+		},{
+			name: "empty content type - should default to text/plain",
+			in: `
+url: http://localhost:3000
+token: 00000000-0000-0000-0000-0000000000001
+content_type: ""
+`,
+		},
+	}
+
+	for _, tt := range test {
+		t.Run(tt.name, func(t *testing.T) {
+			var cfg GotifyConfig
+			err := yaml.UnmarshalStrict([]byte(tt.in), &cfg)
+			require.Equal(t, tt.expected, err)
+		})
+	}
+}
---
+++ notify/gotify/gotify.go
@@ -0,0 +1,146 @@
+package gotify
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"fmt"
+	"log/slog"
+	"net/http"
+	"os"
+	"strconv"
+	"strings"
+
+	commoncfg "github.com/prometheus/common/config"
+
+	"github.com/prometheus/alertmanager/notify"
+	"github.com/prometheus/alertmanager/template"
+	"github.com/prometheus/alertmanager/types"
+)
+
+type Notifier struct {
+	conf    *GotifyConfig
+	tmpl    *template.Template
+	logger  *slog.Logger
+	client  *http.Client
+	retrier *notify.Retrier
+}
+
+func New(c *GotifyConfig, t *template.Template, l *slog.Logger, httpOpts ...commoncfg.HTTPClientOption) (*Notifier, error) {
+	client, err := notify.NewClientWithTracing(*c.HTTPConfig, "gotify", httpOpts...)
+	if err != nil {
+		return nil, err
+	}
+
+	return &Notifier{
+		conf:    c,
+		tmpl:    t,
+		logger:  l,
+		client:  client,
+		retrier: &notify.Retrier{},
+	}, nil
+}
+
+type messageExtrasClientDisplay struct {
+	ContentType string `json:"contentType,omitempty"`
+}
+
+type messageExtras struct {
+	ClientDisplay *messageExtrasClientDisplay `json:"client::display,omitempty"`
+}
+
+type messageRequest struct {
+	Title    string         `json:"title,omitempty"`
+	Message  string         `json:"message"`
+	Priority int            `json:"priority,omitempty"`
+	Extras   *messageExtras `json:"extras,omitempty"`
+}
+
+func (n *Notifier) Notify(ctx context.Context, as ...*types.Alert) (bool, error) {
+	key, err := notify.ExtractGroupKey(ctx)
+	if err != nil {
+		return false, err
+	}
+	logger := n.logger.With("group_key", key)
+	logger.Debug("extracted group key")
+
+	data := notify.GetTemplateData(ctx, n.tmpl, as, logger)
+	var tmplErr error
+	tmplText := notify.TmplText(n.tmpl, data, &tmplErr)
+
+	var token string
+	if n.conf.Token != "" {
+		token = string(n.conf.Token)
+	} else {
+		b, err := os.ReadFile(n.conf.TokenFile)
+		if err != nil {
+			return false, fmt.Errorf("read token_file: %w", err)
+		}
+		token = strings.TrimSpace(string(b))
+	}
+
+	var url string
+	if n.conf.URL != nil {
+		url = n.conf.URL.String()
+	} else {
+		b, err := os.ReadFile(n.conf.URLFile)
+		if err != nil {
+			return false, fmt.Errorf("read url_file: %w", err)
+		}
+		url = strings.TrimSpace(string(b))
+	}
+
+	priority, err := strconv.Atoi(strings.TrimSpace(tmplText(n.conf.Priority)))
+	if err != nil {
+		return false, fmt.Errorf("parse priority: %w", err)
+	}
+
+	req := messageRequest{
+		Title:    strings.TrimSpace(tmplText(n.conf.Title)),
+		Message:  strings.TrimSpace(tmplText(n.conf.Message)),
+		Priority: priority,
+	}
+	if req.Message == "" {
+		req.Message = "(no details)"
+	}
+	if n.conf.ContentType != "" && n.conf.ContentType != "text/plain" {
+		req.Extras = &messageExtras{ClientDisplay: &messageExtrasClientDisplay{ContentType: n.conf.ContentType}}
+	}
+
+	if tmplErr != nil {
+		return false, tmplErr
+	}
+
+	if n.conf.Timeout > 0 {
+		postCtx, cancel := context.WithTimeoutCause(ctx, n.conf.Timeout, fmt.Errorf("configured gotify timeout reached (%s)", n.conf.Timeout))
+		defer cancel()
+		ctx = postCtx
+	}
+
+	var buf bytes.Buffer
+	if err := json.NewEncoder(&buf).Encode(&req); err != nil {
+		return false, err
+	}
+
+	hReq, err := http.NewRequestWithContext(ctx, http.MethodPost, url, &buf)
+	if err != nil {
+		return false, err
+	}
+	hReq.Header.Set("Content-Type", "application/json")
+	hReq.Header.Set("X-Gotify-Key", tmplText(token))
+
+	resp, err := n.client.Do(hReq)
+	if err != nil {
+		if ctx.Err() != nil {
+			err = fmt.Errorf("%w: %w", err, context.Cause(ctx))
+		}
+		return true, notify.RedactURL(err)
+	}
+	defer notify.Drain(resp)
+
+	shouldRetry, err := n.retrier.Check(resp.StatusCode, resp.Body)
+	if err != nil {
+		return shouldRetry, notify.NewErrorWithReason(notify.GetFailureReasonFromStatusCode(resp.StatusCode), err)
+	}
+	return shouldRetry, nil
+}
